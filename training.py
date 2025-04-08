981d15b3d1b54453383c9b5cfb4e3503c30fd1d7
Jatin kumar Balchandani <89150120+jatinavi@users.noreply.github.com>
2025-04-08 22:00:12 +0530

    Update training.py

--- training.py
+++ training.py
@@ -8,19 +8,20 @@ X = []
 Y = []
 
 for i, cat in enumerate(classes):
-	images = glob(f'{path}/{cat}/*.jpeg')
-
-	for image in images:
-		img = cv2.imread(image)
-	
-		X.append(cv2.resize(img, (IMG_SIZE, IMG_SIZE)))
-		Y.append(i)
+    images = glob(f'{path}/{cat}/*.jpeg')
+    
+    for image in images:
+        img = cv2.imread(image)
+        if img is not None:
+            img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+            X.append(img)
+            Y.append(i)
 
 X = np.asarray(X)
 one_hot_encoded_Y = pd.get_dummies(Y).values
 
 
 X_train, X_val, Y_train, Y_val = train_test_split(X, one_hot_encoded_Y,
-												test_size = SPLIT,
-												random_state = 2022)
+                                                  test_size=SPLIT,
+                                                  random_state=2022)
 print(X_train.shape, X_val.shape)
